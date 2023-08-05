6e9c46b8685968123965bf1a12a81e10c7bc8819
WuLiTao <480460995@qq.com>
2023-08-05 14:59:54 +0800

    second commit

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -2,6 +2,7 @@ package com;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello git111111!");
+        System.out.println("hello git222222!");
     }
 }
